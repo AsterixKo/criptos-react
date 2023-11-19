c287a937a7421dbcf417f3450a95b664bef89107
Javier Moneo <jvr.moneo@gmail.com>
2023-11-19 20:27:35 +0100

    Añadiendo un spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 import ImaagenCripto from "./img/imagen-criptos.png";
 import { Formulario } from "./components/Formulario";
 import { Resultado } from "./components/Resultado";
+import { Spinner } from "./components/Spinner";
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -44,9 +45,12 @@ const Heading = styled.h1`
 function App() {
   const [monedas, setMonedas] = useState({});
   const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
+      setCargando(true);
+      setResultado({});
       const cotizarCripto = async () => {
         const { moneda, criptomoneda } = monedas;
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
@@ -54,6 +58,7 @@ function App() {
         const resultado = await respuesta.json();
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+        setCargando(false);
       };
 
       cotizarCripto();
@@ -67,6 +72,8 @@ function App() {
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
         <Formulario setMonedas={setMonedas} />
+
+        {cargando && <Spinner />}
         {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
